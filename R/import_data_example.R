987d01bc99250907d01e74cf6d547ce178774e2e
GuyliannEngels <guyliann.engels@umons.ac.be>
2020-12-11 16:27:18 +0100

    update r script

--- R/import_data_example.R
+++ R/import_data_example.R
@@ -1,25 +1,11 @@
+# Transformation des données brutes en données retravaillées
+
+## Packages utilisés
 SciViews::R
 
+## Importation des données brutes
 
-vec <- 0:20
-t1 <- vec*0.5
+data_raw <- read(...)
 
-
-create_tab <- function(vec, t) {
-  tibble(var1 = vec,
-         ver2 = t * (vec+rnorm(n = length(vec), mean = 0,  sd = 0.0001)))
-}
-
-
-ttt <- create_tab(0:20, 0.2)
-
-t2 <- vec*0.53
-
-t3 <- vec*0.48
-
-
-
-
-t <- fs::dir_ls("data/raw/")
-
-tt <- read(t[2])
+## Sauvegarde du fichier
+write(data_raw, file = "data/...", type = "rds", compress = "xz")
