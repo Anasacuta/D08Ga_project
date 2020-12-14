f5542ae28ac6b50345f3438f6bc0ccd389763ccb
GuyliannEngels <guyliann.engels@umons.ac.be>
2020-12-14 20:03:41 +0100

    update readme

--- R/bodysize_measurements.R
+++ R/bodysize_measurements.R
@@ -1,26 +1,19 @@
 # Importation et remaniement des données brutes
 
-# Packages utilisés
+# Packages utilisés ----
 SciViews::R
-library(purrr)
-library(fs)
 
 # Ces données correspondent en fait à data.io::urchin_bio à peu de choses près
 # (il y a un peu plus de spécimens dans le jeu de données de la publication).
 # Afin de se mettre en situation, nous partons de fichiers bruts en .csv
 # Deux fichiers séparés pour Farm et Fishery
 # On souhaite combiner ces deux jeux de données
-(dir <- dir_ls("data/raw/"))
 
-read_raw_data <- function(path) {
-  res <- read(path)
-  res
-}
+farms <- read()
+fishery <- read()
 
-urchins <- map_dfr(dir, read_raw_data)
+urchins <- bind_rows(famrs, fishery)
 
-# Renommer les variables
-#data <- rename(urchins, )
 
 # Ajouter les labels et unités à chaque colonne du tableau
 # (voir ?data.io::urchin_bio)
